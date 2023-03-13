a87d8c3f84b0863ed1af24a2a7f68f3fc7f9ca06
alexandremakita <83360047+alexandremakita@users.noreply.github.com>
2023-03-13 19:44:28 -0300

    Added files for LAB01b
    Adicionado os exercícios 3 e 5

---
+++ LAB01b/Ex5.c
@@ -0,0 +1,58 @@
+// Ex5 - Multiplicação de linhas ou colunas em uma matriz
+#include <stdio.h>
+
+#define MAX_LINHAS 100
+#define MAX_COLUNAS 100
+
+int main()
+{
+    int matriz[MAX_LINHAS][MAX_COLUNAS];
+    int linhas, colunas, num, opcao, linhacoluna;
+
+    // Definição do tamanho da matriz
+    printf("Digite o número de linhas da matriz: ");
+    scanf("%d", &linhas);
+    printf("Digite o número de colunas da matriz: ");
+    scanf("%d", &colunas);
+
+	// Insreção dos números na matriz
+    printf("Digite os elementos da matriz:\n");
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+    
+    // Inserção do valor de multiplicação
+    printf("Digite o número que deseja multiplicar a linha ou coluna:\n");
+    scanf("%d", &num);
+    
+    // Seleção de operação na linha ou coluna
+    printf("Digite 1 para multiplicar uma linha ou 2 para multiplicar uma coluna:\n");
+    scanf("%d", &opcao);
+    printf("Digite o número da linha/coluna que a operação será feita:\n");
+    scanf("%d", &linhacoluna);
+
+    // Operação de multiplicação
+    if (opcao == 1){
+        for (int j = 0; j < colunas; j++) {
+            matriz[linhacoluna-1][j] *= num;
+        }
+    }
+    if (opcao == 2){
+        for (int i = 0; i < linhas; i++) {
+            matriz[i][linhacoluna-1] *= num;
+        }
+    }
+
+    // Exibição da matriz após a operação
+    printf("A transposta da matriz é:\n");
+    for (int i = 0; i < colunas; i++) {
+        for (int j = 0; j < linhas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
